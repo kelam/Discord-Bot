676bd9973ab5323272971db4b4b807abc451ae82
kelam <kelam@ucsd.edu>
2021-11-02 20:36:49 -0700

    Added reading command files and dynamically executing commands

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+// fs is Node's file system module
+const fs = require("fs");
+
 // Require necessary discord.js classes
-const { Client, Intents } = require("discord.js");
+const { Client, Collections, Intents, ClientUser } = require("discord.js");
 const { token } = require("./config.json");
 
 // Create a new client instance
@@ -7,22 +10,43 @@ const client = new Client({ intents:
     [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES]
 });
 
+// Can access commands in other files
+client.commands = new Collection();
+//fs.readdirSync() returns array of all file names in a directory
+const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
+
+// Dynamically set commands to client.commands
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    //Set a new item in the Collection
+    // With the key as the command name and the value as the exported module
+    client.commands.set(command.data.name, command);
+}
+
 // Runs once the bot is ready
 client.once("ready", () => {
     console.log("The bot is online!");
 });
 
-/* Replying to commands */
+/* Dynamically executing commands */
 
+// Fetch command in Collection and assign to var command
 client.on("interactionCreate", async interaction => {
     if (!interaction.isCommand()) return;
 
-    const { commandName } = interaction;
+    // Fetch command in Collection and assign to var command
+    const command = client.commands.get(interaction.commandName);
 
-    if (commandName === "server") {
-        await interaction.reply(`Server info name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
-    } else if (commandName === "user") {
-        await interaction.reply("User info");
+    // If command doesn't exist, returns undefined
+    if (!command) return;
+
+    // If command does exist, call command's .execute() and pass in the interaction var as its argument
+    try {
+        await command.execute(interaction);
+        // Logs any error
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: "There was an error while executing this command!", ephemeral: true});
     }
 });
 
