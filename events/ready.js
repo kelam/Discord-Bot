39f74ce1a65880476fec0999f453d7365200ca71
kelam <kelam@ucsd.edu>
2021-11-02 21:07:04 -0700

    Add event files

---
+++ events/ready.js
@@ -0,0 +1,9 @@
+module.exports = {
+    // Name states which event this file is for
+    name: "ready",
+    once: true,
+    // For event logic, which is called by event handler whenever the event emits
+    execute(client) {
+        console.log(`Ready! Logged in as ${client.user.tag}`);
+    },
+};
