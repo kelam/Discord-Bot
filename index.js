8397a1ff7e7a425727efafa4df618f439ac10e2d
kelam <kelam@ucsd.edu>
2021-11-02 19:09:50 -0700

    Added replies to commands

--- index.js
+++ index.js
@@ -12,6 +12,20 @@ client.once("ready", () => {
     console.log("The bot is online!");
 });
 
+/* Replying to commands */
+
+client.on("interactionCreate", async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+
+    if (commandName === "server") {
+        await interaction.reply(`Server info name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+    } else if (commandName === "user") {
+        await interaction.reply("User info");
+    }
+});
+
 // Login to Discord with my bot's token
 client.login(token);
 
